829fc900f4459ce1cd60a4a0057faf76d62630f6
MilicaNn <milicaaanikoliccc99@gmail.com>
2024-02-09 21:09:53 +0100

    proj...1234556456456.

--- src/app/accomodation/rooms/double/double.component.ts
+++ src/app/accomodation/rooms/double/double.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-double',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./double.component.css']
 })
 export class DoubleComponent {
+  translatedParagraph?: SafeHtml;
 
+  constructor(private translate: TranslateService, private sanitizer: DomSanitizer) {}
+
+  ngOnInit(): void {
+      this.translate.get('PARAGRAPH3').subscribe((res: string) => {
+          this.translatedParagraph = this.sanitizer.bypassSecurityTrustHtml(res);
+      });
+  }
 }
